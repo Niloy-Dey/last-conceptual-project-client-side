0d0042e8e3b1051824347b033d027afc8dfa23ea
Niloy-Dey <niloydeyce@gmail.com>
2022-05-17 23:09:41 +0600

    theme change proble is solved

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -3,10 +3,10 @@ import banner from '../../assets/bucketgirl 1.png'
 const Landing = () => {
     return (
         <div>
-            <div class="hero h-screen lg:h-[60vh] bg-accent mt-20 ">
+            <div class="hero h-screen lg:h-[70vh] bg-accent mt-20 ">
                 <div class="hero-content flex-col lg:flex-row-reverse">
-                    <div className='h-[60vh] shrink-0'>
-                    <img src={banner} class="h-full"  alt=''/>
+                    <div className='h-[50vh] shrink-0'>
+                        <img src={banner} class="h-full" alt='' />
                     </div>
                     <div>
                         <h1 class="text-5xl font-bold">Box Office News!</h1>
@@ -17,14 +17,19 @@ const Landing = () => {
             </div>
 
 
-            <div className='shadow-lg p-10 w-5/6 rounded-lg mt-[-20px] mx-auto'>
-                <h1>GET Free Estimate </h1>
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
-                <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+            <div className='shadow-lg p-10 w-5/6 bg-base-200 mb-28 rounded-lg mt-[-30px] mx-auto'>
+                <h1 className='text-xl font-bold p-1 text-primary sm:text-center lg:text-left' >GET Free Estimate </h1>
+                <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4  justify-center' >
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                    <input placeholder='type Here' className='input input-bordered w-full max-w-xs' type="text" name="" id="" />
+                </div>
+                <button class="btn btn-primary mt-2">Get Started</button>
             </div>
         </div>
     );
